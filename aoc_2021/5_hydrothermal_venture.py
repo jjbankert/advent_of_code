993051667216302f96924d6c7d1b81421a487646
Jelle Jan Bankert <jjbankert@gmail.com>
2021-12-05 14:06:59 +0100

    added numpy linspace solution to 2021, 5

--- aoc_2021/5_hydrothermal_venture.py
+++ aoc_2021/5_hydrothermal_venture.py
@@ -9,31 +9,50 @@ def main():
     vent_lines = []
     for line in data:
         start, end = line.split(' -> ')
-        start_position = tuple(int(number) for number in start.split(','))
-        end_position = tuple(int(number) for number in end.split(','))
+        start_position = numpy.fromstring(start, dtype='int32', sep=',')
+        end_position = numpy.fromstring(end, dtype='int32', sep=',')
         vent_lines.append((start_position, end_position))
 
-    counter = Counter()
+    exact_counter = Counter()
+    linspace_counter = Counter()
     for line in vent_lines:
         start, end = line
-        if start[0] == end[0]:
-            lower_range = min(start[1], end[1])
-            upper_range = max(start[1], end[1]) + 1
-            length = upper_range - lower_range
-            counter.update(zip([start[0]] * length, range(lower_range, upper_range)))
-        elif start[1] == end[1]:
-            lower_range = min(start[0], end[0])
-            upper_range = max(start[0], end[0]) + 1
-            length = upper_range - lower_range
-            counter.update(zip(range(lower_range, upper_range), [start[1]] * length))
-        else:
-            col_direction = (end[0] - start[0]) // abs(end[0] - start[0])
-            col_range = range(start[0], end[0] + col_direction, col_direction)
-            row_direction = (end[1] - start[1]) // abs(end[1] - start[1])
-            row_range = range(start[1], end[1] + row_direction, row_direction)
-            counter.update(zip(col_range, row_range))
 
-    dangerous_positions = {key: value for key, value in counter.items() if value > 1}
+        # linspace
+        number_of_steps = max([abs(end[0] - start[0]), abs(end[1] - start[1])]) + 1
+        linspace_positions = [
+            tuple(position)
+            for position
+            in numpy.rint(numpy.linspace(start, end, num=number_of_steps)).astype('int32')
+        ]
+
+        # exact
+        # if start[0] == end[0]:
+        #     lower_range = min(start[1], end[1])
+        #     upper_range = max(start[1], end[1]) + 1
+        #     length = upper_range - lower_range
+        #     exact_positions = list(zip([start[0]] * length, range(lower_range, upper_range)))
+        # elif start[1] == end[1]:
+        #     lower_range = min(start[0], end[0])
+        #     upper_range = max(start[0], end[0]) + 1
+        #     length = upper_range - lower_range
+        #     exact_positions = list(zip(range(lower_range, upper_range), [start[1]] * length))
+        # else:
+        #     col_direction = (end[0] - start[0]) // abs(end[0] - start[0])
+        #     col_range = range(start[0], end[0] + col_direction, col_direction)
+        #     row_direction = (end[1] - start[1]) // abs(end[1] - start[1])
+        #     row_range = range(start[1], end[1] + row_direction, row_direction)
+        #
+        #     exact_positions = list(zip(col_range, row_range))
+
+        # exact_counter.update(exact_positions)
+        linspace_counter.update(linspace_positions)
+
+    # debugging differences between exact and linspace counters
+    # print(f"{exact_counter.most_common(10)=}")
+    # print(f"{linspace_counter.most_common(10)=}")
+
+    dangerous_positions = {key: value for key, value in linspace_counter.items() if value > 1}
     print(len(dangerous_positions))
 
 
