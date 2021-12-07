baa9f57999a6438197874ecd5f21b6b1997cefd5
Jelle Jan Bankert <jellejanbankert@godatadriven.com>
2021-12-07 18:56:00 +0100

    2021, 7

---
+++ aoc_2021/7_treachery_of_whales.py
@@ -0,0 +1,43 @@
+import numpy as np
+
+from aoc_2021 import load_data
+
+
+def main():
+    data = np.fromstring(
+        load_data(__file__, splitlines=False).strip(), dtype="int", sep=","
+    )
+
+    # part 1
+    idx_costs = {
+        candidate_idx: np.sum(np.abs(data - candidate_idx))
+        for candidate_idx in range(data.min(), data.max() + 1)
+    }
+    cheapest_idx(idx_costs)
+
+    # part 2
+    idx_costs = {
+        candidate_idx: np.sum(gauss_sum(data - candidate_idx))
+        for candidate_idx in range(data.min(), data.max() + 1)
+    }
+
+    cheapest_idx(idx_costs)
+
+
+@np.vectorize
+def gauss_sum(steps):
+    steps = abs(steps)
+    return steps * (steps + 1) / 2
+
+
+def cheapest_idx(idx_costs):
+    lowest_idx, lowest_cost = next(iter(idx_costs.items()))
+    for row_idx, row_cost in idx_costs.items():
+        if row_cost < lowest_cost:
+            lowest_idx, lowest_cost = row_idx, row_cost
+
+    print(f"{lowest_idx=}, {lowest_cost=}")
+
+
+if __name__ == "__main__":
+    main()
